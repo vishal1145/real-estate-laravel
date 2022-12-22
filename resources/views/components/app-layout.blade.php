62c0d07323aa84a3c9a621dd745aefcfedad8068
vishal1145 <vishal.kumar1145@gmail.com>
2022-12-22 16:44:06 +0530

    test

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -16,13 +16,10 @@
     </title>
 
     <!-- CSS & JS Assets -->
-    
+    <link rel="stylesheet" href="{{resources('css/app.css')}}">
+
     {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
-
-    {{ HTML::script('resources/js/app.js') }}
-
-{{ HTML::style('resources/css/app.css') }}
-
+    
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
